7345146414512e6ee11e4d9f26c7af44cf0d33d9
wzhao1 <wzhao1@andrew.cmu.edu>
2019-01-08 01:31:27 -0500

    feat:expr on 3lines_2 with linear, dt
    1. Linear regression
    2. Decision tree with grid-search param tuning

--- src/models/models.py
+++ src/models/models.py
@@ -5,6 +5,23 @@ import sklearn
 import pdb
 
 
+def get_linear(params):
+    '''
+    Parameters
+    ----------
+    params: dict
+
+    Returns
+    -------
+    model: sklearn model
+    '''
+    from sklearn.linear_model import LinearRegression
+
+    model = LinearRegression(**params)
+
+    return model
+
+
 def get_svr(params):
     '''
     Parameters
@@ -13,10 +30,27 @@ def get_svr(params):
 
     Returns
     -------
-    model: sklearn.svm.SVR
+    model: sklearn model
     '''
     from sklearn.svm import SVR
 
     model = SVR(**params)
+
+    return model
+
+
+def get_decision_tree(params):
+    '''
+    Parameters
+    ----------
+    params: dict
+
+    Returns
+    -------
+    model: sklearn model
+    '''
+    from sklearn.tree import DecisionTreeRegressor
+
+    model = DecisionTreeRegressor(**params)
 
     return model
