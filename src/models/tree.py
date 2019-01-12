eaebc2867b52fc5b449d75f4bf5a8a3ea008ab81
wzhao1 <wzhao1@andrew.cmu.edu>
2019-01-12 15:19:49 -0500

    feat & style: cv, logger, doc, clean-up

--- src/models/tree.py
+++ src/models/tree.py
@@ -4,6 +4,7 @@
 import numpy as np
 import scipy
 from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
 import pdb
 
 
@@ -27,14 +28,13 @@ class Node():
         if self.is_root:  # root node
             self.parent = None
             self.children = []
-
             self.children_count = 0
-            self.child_left = None
-            self.child_right = None
+
         else:
             if not self.is_leaf:  # non-root non-leaf node
                 self.parent = parent
                 self.children = []
+                self.children_count = 0
 
                 parent.children.append(self)
                 parent.children_count += 1
@@ -48,23 +48,28 @@ class Node():
 
         # Initialize node-expert depending on is_leaf or not
         if not self.is_leaf:  # non-leaf node
-            self.classifier = SVC(self.args['svm'])
+            #  self.classifier = SVC(**self.args['svm'])
+            self.classifier = SVC(**self.args['preset_model_params'])
+
+            self.classifier_best = None
+            self.t_best = None
+
         else:  # leaf node
-            self.w = np.random.standard_normal((self.feature_dim + 1,))  # include bias
-            self.sigma = np.random.uniform(0, 1)  # TODO: no-correlation?
+            self.w = np.random.standard_normal((self.feature_dim + 1,))  # NOTE: include bias
+            self.sigma = np.random.uniform(0, 1)  # NOTE: no-correlation
 
-        # Initialize likelihood depending on is_leaf or not
-        if not self.is_leaf:  # non-leaf node
-            self.qz_l = None  # q(z->left | x)
-            self.qz_r = None  # q(z->right | x)
-        else:  # leaf node
-            self.py_xz = None  # P(y | x, z)
+            self.w_best = None
+            self.sigma_best = None
 
-        self.classifier_best = None  # TODO: storing?
-        self.w_best = None
-        self.sigma_best = None
+        # Initialize node conditional and criterion
+        if self.is_root:  # root
+            self.qz_x_best = 1.  # q(z->current node | x)
+            self.Qk_best = -1e9
+        else:  # non-root
+            self.qz_x_best = None
+            self.Qk_best = None
 
-    def convert_leaf_node(self):
+    def convert_leaf2nonleaf(self):
         '''
         Convert leaf node to non-leaf node.
         '''
@@ -72,13 +77,22 @@ class Node():
         self.children = []
         self.children_count = 0
 
-        self.classifier = SVC(self.args['svm'])
-        self.qz_l = None  # q(z->left | x)
-        self.qz_r = None  # q(z->right | x)
+        # Create non-leaf data
+        #  self.classifier = SVC(**self.args['svm'])
+        self.classifier = SVC(**self.args['preset_model_params'])
+        self.classifier_best = None
+        self.t_best = None
 
-        self.w = None
-        self.sigma = None
-        self.py_xz = None  # P(y | x, z)
+    def convert_nonleaf2leaf(self):
+        '''
+        Convert non-leaf node to leaf node.
+        '''
+        assert self.classifier_best is None, "Attempting to convert a splitter node to leaf node"
+        self.is_leaf = True
+        self.children = None
+
+        # Null non-leaf data
+        self.classifier = None
 
     def get_parent(self):
         assert self.parent is not None, "No parent! check if it is a root node"
@@ -104,29 +118,22 @@ class Tree():
     Binary tree.
     '''
 
-    def __init__(self, args=None):
+    def __init__(self, logger=None, args=None):
         assert args is not None, "No arguments provided!"
         self.args = args
+        self.logger = logger
 
         np.random.seed(self.args['random_seed'])
 
-        self.nodes = []
-        self.nodes_per_level = []
+        self.data = None  # store original data
+        self.num_samples = None  # total number of samples
 
-        self.root = self.create_root()
+        self.nodes = []  # store nodes on the tree
+
+        self.root = self.create_root()  # create root
         self.nodes.append(self.root)
-        self.nodes_per_level.append(self.root)
 
         self.t_best_list = []  # collection of best node thresholds
-        self.t_best = None
-        self.t_current = np.random.uniform(args['t_low'], args['t_high'])
-
-        self.Q_best = -1e9  # expectation of complete data log-likelihood
-        self.Q_best_list = []
-
-        self.nodes_best = []
-
-        self.tol = 0.1
 
     def create_root(self):
         root = Node(is_root=True, args=self.args)
@@ -151,107 +158,140 @@ class Tree():
 
     # ========================================
     def train(self, X, Y):
+        '''
+        Train the tree model.
+        '''
+        self.data = (X, Y)
+        self.num_samples = len(Y)
 
         self.grow([X], [Y], [self.root])
 
     def grow(self, X, Y, nodes_per_level):
+        '''
+        Recursively grow the tree.
+
+        Parameters
+        ----------
+        X: Tuple(np.array), shape (N, D)
+        Y: Tuple(np.array), shape (N,)
+        nodes_per_level: List[Node]
+        '''
         for i, node in enumerate(nodes_per_level):
 
+            # Get data
             Xi, Yi = X[i], Y[i]
 
-            node.convert_leaf_node()  # TODO: is this correct???
+            #  if len(Yi) < self.args['min_samples_split']:
+                #  # node has insufficient data to split
+                #  continue  # do not split
+
+            if node.is_root is not True:
+                # convert leaf to non-leaf node
+                node.convert_leaf2nonleaf()
+
+            # Grow subtree
             node_left, node_right = self.grow_subtree(node)
             self.nodes.append(node_left)
             self.nodes.append(node_right)
+            self.logger.debug('=' * 50)
+            self.logger.debug('grown a subtree')
 
+            # Optimize subtree
             Q_best, best_collection = self.M_step(Xi, Yi, node, node_left, node_right)
 
-            if Q_best > self.Q_best:
+            if Q_best > node.Qk_best:  # if improvement, keep growing
+            #  if Q_best > node.Qk_best and Q_best - node.Qk_best < self.args['min_Q_increase']:
 
-                self.Q_best = Q_best
-                node_best = best_collection['node']
-                node_left_best = best_collection['node_left']
-                node_right_best = best_collection['node_right']
-
-                self.Q_best_list.append(Q_best)
                 self.t_best_list.append(best_collection['t'])
-                self.nodes_best.append(node_best)
-                self.nodes_best.append(node_left_best)
-                self.nodes_best.append(node_right_best)
 
-                node_best.classifier_best = node_best.classifier
-                node_left_best.w_best = node_left_best.w
-                node_right_best.w_best = node_right_best.w
-                # TODO: store node proba?
-
-                Q_increase = Q_best - self.Q_best
-
-                if Q_increase < self.tol:
-                    break
-                #  return best_collection  # TODO: ??
-
-                nodes_per_level_new = [node_left_best, node_right_best]
                 X_new = (best_collection['data_left'][0], best_collection['data_right'][0])
                 Y_new = (best_collection['data_left'][1], best_collection['data_right'][1])
+                nodes_per_level_new = [best_collection['node_left'],
+                                       best_collection['node_right']]
 
+                self.logger.debug('*' * 50)
+                self.logger.debug('subtree saw improvement')
+                self.logger.debug('t_best: {:.4f}  Q_best: {:.4f}'.format(best_collection['t'], Q_best))
                 self.grow(X_new, Y_new, nodes_per_level_new)
 
-            else:
+            else:  # no improvement with this subtree
+                node.convert_nonleaf2leaf()  # convert back to leaf
+                del self.nodes[-1]
+                del self.nodes[-1]
+                self.logger.debug('*' * 50)
+                self.logger.debug('subtree has no improvement, deleted')
                 continue
 
     # ========================================
-    def E_step(self):
-        self.evaluate_Q()
-
     def evaluate_Q(self, node, node_left, node_right, X, Y, t):
 
-        qz_x_left, qz_x_right = self._infer_Qz_x(node, X)
-        py_xz_left = self._infer_Py_xz(node_left, X, Y)
-        py_xz_right = self._infer_Py_xz(node_right, X, Y)
+        Xo, Yo = self.data  # get all original data
+
+        # q(z->left | x) q(z->right | x)
+        if node.is_root is True:
+            qz_x_prev = 1.
+        else:  # get previous conditional
+            qz_x_prev = node.qz_x_best
+
+        qz_x_left_current, qz_x_right_current = self._infer_Qz_x(node, Xo)
+        qz_x_left = qz_x_prev * qz_x_left_current
+        qz_x_right = qz_x_prev * qz_x_right_current
+
+        # p(y | x, z->left) p(y | x, z->right)
+        py_xz_left = self._infer_Py_xz(node_left, Xo, Yo)
+        py_xz_right = self._infer_Py_xz(node_right, Xo, Yo)
+
+        # p(z->left | x) p(z->right | x)
         pz_x_left, pz_x_right = self._infer_Pz_x(Y, t)
 
-        # SUM
-        Q = (qz_x_left * (np.log(pz_x_left * py_xz_left) -
-                        np.log(qz_x_left)) +
-             qz_x_right * (np.log(pz_x_right * py_xz_right) -
-                        np.log(qz_x_right)))
+        # Sum
+        Q_left = np.sum(qz_x_left * (np.log(pz_x_left * py_xz_left + 1e-9) - np.log(qz_x_left + 1e-9)))
+        Q_right = np.sum(qz_x_right * (np.log(pz_x_right * py_xz_right + 1e-9) - np.log(qz_x_right + 1e-9)))
+        Q = Q_left + Q_right
 
-        return Q
+        return Q, Q_left, Q_right, qz_x_left, qz_x_right
 
-    def _infer_Pz_x(self, Y, t):  # TODO: local or global?
+    def _infer_Pz_x(self, Y, t):
         '''
         Compute p(z=1 | x, t) and p(z=0 | x, t).
-        '''
-        N = len(Y)
-        n_le = np.count_nonzero(Y <= t)
 
-        pz_x_left = n_le / float(N)
-        pz_x_right = 1 - pz_x_left
+        NOTE: using global approximation
+        '''
+        n_le = np.count_nonzero(Y <= t)
+        n_gt = np.count_nonzero(Y > t)
+
+        pz_x_left = n_le / float(self.num_samples)
+        pz_x_right = n_gt / float(self.num_samples)
 
         return pz_x_left, pz_x_right
 
     def _infer_Py_xz(self, node, X, Y):
-        N = X.shape[0]
+        '''
+        Compute p(y | x, z).
+        '''
+        X = self._add_ones(X)
+
         mu = np.dot(X, node.w)
-        std = np.eye(N) * np.sqrt(node.sigma)
+        std = np.ones(mu.shape) * np.sqrt(node.sigma)
+
         py_xz = scipy.stats.norm(mu, std).pdf(Y)
         return py_xz
 
     def _infer_Qz_x(self, node, X):
-        qz_x_left, qz_x_right = node.svm.predict_proba(X)
+        '''
+        Compute q(z=0 | x) and q(z=1 | x).
+        '''
+        probs = node.classifier.predict_proba(X)
+        qz_x_left = probs[:, 0]
+        qz_x_right = probs[:, 1]
         return qz_x_left, qz_x_right
 
     # ========================================
-    def M_step(self, X, Y, node, node_left, node_right):  # TODO: data per node is different
-        #  t_grid = np.arange(
-            #  self.args['t_low'],
-            #  self.args['t_high'] + self.args['t_step'],
-            #  self.args['t_step'])
+    def M_step(self, X, Y, node, node_left, node_right):
 
-        t_step = self.args['t_step']
-        t_grid = np.arange(np.min(Y) + t_step, np.min(Y) - t_step, t_step)
+        # Initialize best settings
+        Q_best = node.Qk_best  # init with current sub_root's best Q
 
-        Q_best = self.Q_best
         best_collection = {
             "t": None,
             "Q": None,
@@ -262,21 +302,44 @@ class Tree():
             "node_right": None
         }
 
+        # Get t
+        t_step = self.args['t_step']
+        t_low = np.min(Y) + t_step
+        t_high = np.max(Y) - t_step
+
+        #  if t_high - t_low <= 0:
+            #  return Q_best, best_collection
+
+        t_grid = np.arange(t_low, t_high, t_step)
+
         for t in t_grid:
+            # Process data
+            data_left, data_right = self._split_data(X, Y, t)
+
+            #  if len(data_left[1]) < self.args['min_samples_leaf'] or \
+               #  len(data_right[1]) < self.args['min_samples_leaf']:
+                #  # leaf has insufficient data to split
+                #  continue  # proceed to next t
+
+            self.logger.debug('t: {:.4f}'.format(t))
             Y_new = self._create_new_label(Y, t)
-            self._train_svm(node, X, Y_new)
 
-            data_left, data_right = self.split_data(X, Y, t)
+            # Train classifier
+            self.train_svm(node, X, Y_new)
 
-            self._train_leaf(node_left, *data_left)
-            self._train_leaf(node_right, *data_right)
+            # Train regressor
+            self.train_leaf(node_left, *data_left)
+            self.train_leaf(node_right, *data_right)
 
-            Q = self.evaluate_Q(node, node_left, node_right,
-                            X, Y, t)
+            # Eveluate Q
+            Q, Q_left, Q_right, qz_x_left, qz_x_right =\
+                self.evaluate_Q(node, node_left, node_right, X, Y, t)
+            self.logger.debug('Q: {:.4f}  Q_left: {:.4f}  Q_right: {:.4f}'.format(Q, Q_left, Q_right))
 
             if Q > Q_best:
                 Q_best = Q
 
+                # store current best settings
                 best_collection['t'] = t
                 best_collection['Q'] = Q
                 best_collection['data_left'] = data_left
@@ -284,6 +347,24 @@ class Tree():
                 best_collection['node'] = node
                 best_collection['node_left'] = node_left
                 best_collection['node_right'] = node_right
+
+                # store best node classifier
+                node.classifier_best = node.classifier
+                node.t_best = t
+
+                # store best leaf regressor
+                node_left.w_best = node_left.w
+                node_left.sigma_best = node_left.sigma
+                node_right.w_best = node_right.w
+                node_right.sigma_best = node_right.sigma
+
+                # store best node conditional
+                node_left.qz_x_best = qz_x_left
+                node_right.qz_x_best = qz_x_right
+
+                # store best node criterion
+                node_left.Qk_best = Q_left
+                node_right.Qk_best = Q_right
 
         return Q_best, best_collection
 
@@ -301,10 +382,11 @@ class Tree():
         Y_new: np.array[{0, 1}], shape (N,)
         '''
         Y_new = np.array(list(map(lambda y, x: 0 if y <= x else 1,
-                                  Y, np.ones(Y.shape) * t)))
+                                  Y, np.ones(Y.shape) * t)),
+                         dtype=float)
         return Y_new
 
-    def split_data(self, X, Y, t):
+    def _split_data(self, X, Y, t):
         '''
         Split data w.r.t. Y <= t or Y > t.
 
@@ -318,8 +400,8 @@ class Tree():
         -------
         (X_, Y_): (np.array, np.array)
         '''
-        idx_le = np.nonzero(Y <= t)
-        idx_gt = np.nonzero(Y > t)
+        idx_le = np.nonzero(Y <= t)[0]
+        idx_gt = np.nonzero(Y > t)[0]
 
         X_left = np.take(X, idx_le, axis=0)
         Y_left = np.take(Y, idx_le, axis=0)
@@ -329,16 +411,35 @@ class Tree():
 
         return (X_left, Y_left), (X_right, Y_right)
 
-    def _train_svm(self, node, X, Y):
-        assert node.is_leaf is not True, "Error: leaf node reached!"
-
-        node.classifier.fit(X, Y)
-
     def _add_ones(self, X):
-        N = X.shape(0)
+        '''
+        Add one column of 1s before X.
+        '''
+        N = X.shape[0]
         return np.concatenate((np.ones((N, 1)), X), axis=1)
 
-    def _train_leaf(self, node, X, Y):
+    def train_svm(self, node, X, Y):
+        '''
+        Train SVM.
+        '''
+        assert node.is_leaf is not True, "Error: leaf node reached!"
+        # node.classifier.fit(X, Y)
+
+        svm = node.classifier
+        grid = GridSearchCV(svm, **self.args['tuning_settings'])
+        grid.fit(X, Y)
+
+        self.logger.info('-' * 50)
+        self.logger.info('Best parameters: ')
+        self.logger.info(grid.best_params_)
+        pdb.set_trace()
+        grid.predict()
+        grid.best_estimator_
+
+    def train_leaf(self, node, X, Y):
+        '''
+        Train leaf.
+        '''
         # TODO: N >> D, non-invertible case?
         assert node.is_leaf is True, "Error: non-leaf node!"
 
@@ -351,14 +452,31 @@ class Tree():
         node.w = W
 
         N = len(Y)
-
         sigma = np.sum(np.square(Y - X.dot(W))) / (N - 2)
         node.sigma = sigma
 
     # ========================================
-    def predict(self):
-        pass
+    def predict(self, X):
+        Yh = []
+        for x in X:
+            x = x.reshape(-1, 1)
 
+            # Trace down to leaf node
+            node = self.root
+            while not node.is_leaf:
+                cls = node.classifier_best.predict(x)
+                if cls <= 0:  # -> left node
+                    node = node.children[0]
+                else:  # -> right node
+                    node = node.children[1]
+
+            x = self._add_ones(x)
+            yh = np.dot(x, node.w_best)
+            Yh.append(yh)
+
+        return np.array(Yh, dtype=float)
+
+    # ========================================
     def get_path(self):
         pass
 
--- src/models/tree_test.py
+++ src/models/tree_test.py
@@ -4,6 +4,11 @@
 import os
 import sys
 import json
+import logging
+import logging.config
+import numpy as np
+from sklearn import metrics
+from matplotlib import pyplot as plt
 import pdb
 for path in [
         'data_utils',
@@ -26,6 +31,19 @@ if len(sys.argv) < 2:
 with open(sys.argv[1], 'r') as f:
     args = json.load(f)
 
+# Log
+if not os.path.exists(args['log_dir']):
+    os.makedirs(args['log_dir'])
+
+log_file = os.path.join(args['log_dir'], args['log_file'])
+if os.path.isfile(log_file):  # remove existing log
+    os.remove(log_file)
+
+logging.config.dictConfig(args['log'])  # setup logger
+logger = logging.getLogger('main')
+
+# Data
+logger.info('Loading & preparing data')
 opts = args['3lines_2']
 X_train, Y_train = get_3lines_2_data(
     os.path.join(opts['path'], opts['train']['file'])
@@ -38,10 +56,44 @@ if X_train.ndim < 2:
     X_train = X_train.reshape(-1, 1)
     X_test = X_test.reshape(-1, 1)
 
+logger.info('Initializing model')
 tree_opts = args['beta']
-tree = Tree(tree_opts)
+tree = Tree(logger=logger, args=tree_opts)
 
-tree.grow_subtree(tree.root)
-tree.nodes[1].convert_leaf_node()
-tree.grow_subtree(tree.nodes[1])
+logger.info('Training')
+tree.train(X_train, Y_train)
+Yp_train = tree.predict(X_train)
+Yp_test = tree.predict(X_test)
+mae_train = metrics.mean_absolute_error(Y_train, Yp_train)
+rmse_train = np.sqrt(metrics.mean_squared_error(Y_train, Yp_train))
+mae_test = metrics.mean_absolute_error(Y_test, Yp_test)
+rmse_test = np.sqrt(metrics.mean_squared_error(Y_test, Yp_test))
+print('Train: ', mae_train, rmse_train)
+print('Test: ', mae_test, rmse_test)
+
+# Plot data
+fig = plt.figure(figsize=(6, 6))
+ax1 = fig.add_subplot(111)
+ax1.scatter(X_train, Y_train, s=2, c='b', marker='.')
+ax1.scatter(X_train, Yp_train, s=2, c='r', marker='+')
+ax1.axis('tight')
+ax1.tick_params(axis='both', which='major', labelsize=12)
+ax1.set_xlabel('x', fontsize=12)
+ax1.set_ylabel('y', fontsize=12)
+ax1.legend(('original', 'pred'), loc='best', fontsize=11)
+plt.tight_layout()
+plt.savefig('3lines_2_tree_train.pdf')
+
+fig = plt.figure(figsize=(6, 6))
+ax1 = fig.add_subplot(111)
+ax1.scatter(X_test, Y_test, s=2, c='b', marker='.')
+ax1.scatter(X_test, Yp_test, s=2, c='r', marker='+')
+ax1.axis('tight')
+ax1.tick_params(axis='both', which='major', labelsize=12)
+ax1.set_xlabel('x', fontsize=12)
+ax1.set_ylabel('y', fontsize=12)
+ax1.legend(('original', 'pred'), loc='best', fontsize=11)
+plt.tight_layout()
+plt.savefig('3lines_2_tree_test.pdf')
+
 pdb.set_trace()
